e8b2c34b1074ce5ef1a1fb331636d97743d9538b
mike213112 <mikeramirez-14-31@hotmail.com>
2020-09-18 19:34:31 -0600

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ChatComponent } from './componentes/chat/chat.component';
+import { PublicacionComponent } from './componentes/modals/publicacion/publicacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    NavbarComponent,
+    ChatComponent,
+    PublicacionComponent
   ],
   imports: [
     BrowserModule,
